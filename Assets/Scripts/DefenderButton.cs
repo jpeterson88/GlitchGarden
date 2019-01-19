837c8f651164e68f7ac0bb0e6da352130a12a8eb
Justin Peterson <jpeterson4223@yahoo.com>
2019-01-19 09:02:10 -0800

    Added Defender buttons to bottom of screen and highlight on click

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<DefenderButton>();
+        foreach (DefenderButton button in buttons)
+        {
+            button.ChangeButtonColor(new Color32(108, 108, 108, 108));
+        }
+
+        ChangeButtonColor(Color.white);
+    }
+
+    private void ChangeButtonColor(Color color)
+    {
+        GetComponent<SpriteRenderer>().color = color;
+    }
+}
--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Defender : MonoBehaviour
+public class DefenderSpawner : MonoBehaviour
 {
+    [SerializeField] GameObject defender;
     private void OnMouseDown()
     {
-        Debug.Log("Mouse is clicked");
+        SpawnDefender(GetSquareClicked());
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickedPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(clickedPosition);
+
+        return SnapToGrid(worldPosition);
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPosition)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPosition.x);
+        float newY = Mathf.RoundToInt(rawWorldPosition.y);
+
+        return new Vector2(newX, newY);
+    }
+
+    private void SpawnDefender(Vector2 roundedPosition)
+    {
+        GameObject newDefender = Instantiate(defender, roundedPosition, Quaternion.identity) as GameObject;
     }
 }
