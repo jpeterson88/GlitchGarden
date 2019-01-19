4d78e0c4c6577336846477b3159d187ef7f10c19
Justin Peterson <jpeterson4223@yahoo.com>
2019-01-18 16:10:30 -0800

    added spawn cactus on click location

---
+++ Assets/Scripts/Defender.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject defender;
+    private void OnMouseDown()
+    {
+        SpawnDefender(GetSquareClicked());
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickedPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(clickedPosition);
+
+        return worldPosition;
+    }
+
+    private void SpawnDefender(Vector2 worldPosition)
+    {
+        GameObject newDefender = Instantiate(defender, worldPosition, Quaternion.identity) as GameObject;
+    }
+}
