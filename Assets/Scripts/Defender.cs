c496187d8eddcc13b60720ee1670ff3b2714a385
Justin Peterson <jpeterson4223@yahoo.com>
2019-01-19 16:48:28 -0800

    Added star render from trophy

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -6,4 +6,8 @@ public class Defender : MonoBehaviour
 {
     [SerializeField] int starCost = 100;
 
+    public void AddStars(int starsToAdd)
+    {
+        FindObjectOfType<StarDisplay>().AddStars(starsToAdd);
+    }
 }
